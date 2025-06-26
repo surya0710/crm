26b1d3f607477a6e23f78a4e889db8e33644cda5
Surya <157574400+0710surya@users.noreply.github.com>
2025-06-26 13:29:08 +0530

    Added forgot password module

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,25 +1,75 @@
-<x-guest-layout>
-    <div class="mb-4 text-sm text-gray-600 dark:text-gray-400">
-        {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Forgot Password</title>
+    <!-- Favicon icon-->
+    <link rel="icon" href="{{ asset('assets/images/favicon.png ') }}" type="image/x-icon">
+    <link rel="shortcut icon" href="{{ asset('assets/images/favicon.png') }}" type="image/x-icon">
+    <!-- Google font-->
+    <link rel="preconnect" href="https://fonts.googleapis.com/">
+    <link rel="preconnect" href="https://fonts.gstatic.com/" crossorigin="">
+    <link href="https://fonts.googleapis.com/css2?family=Nunito+Sans:opsz,wght@6..12,200;6..12,300;6..12,400;6..12,500;6..12,600;6..12,700;6..12,800;6..12,900;6..12,1000&amp;display=swap" rel="stylesheet">
+    <!-- Flag icon css -->
+    <link rel="stylesheet" href="{{ asset('assets/css/vendors/flag-icon.css') }}">
+    <!-- iconly-icon-->
+    <link rel="stylesheet" href="{{ asset('assets/css/iconly-icon.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/bulk-style.css') }}">
+    <!-- iconly-icon-->
+    <link rel="stylesheet" href="{{ asset('assets/css/themify.css') }}">
+    <!--fontawesome-->
+    <link rel="stylesheet" href="{{ asset('assets/css/fontawesome-min.css') }}">
+    <!-- Whether Icon css-->
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/vendors/weather-icons/weather-icons.min.css') }}">
+    <!-- App css -->
+    <link id="color" rel="stylesheet" href="{{ asset('assets/css/color-1.css') }}" media="screen">
+    <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
+  </head>
+  <body>
+    <!-- tap on top starts-->
+    <div class="tap-top"><i class="iconly-Arrow-Up icli"></i></div>
+    <!-- tap on tap ends-->
+    <!-- login page start-->
+    <div class="container-fluid p-0">
+      <div class="row m-0">
+        <div class="col-12 p-0">    
+          <div class="login-card login-dark">
+            <div>
+              <div class="login-main"> 
+                <form class="theme-form" action="{{ route('password.email') }}" method="POST">
+                  @csrf
+                  <h2 class="text-center">Forgot Password</h2>
+                  <p class="text-center">Enter your email to reset your password</p>
+                  <x-auth-session-status class="text-center mt-4 text-success" :status="session('status')" />
+                  <div class="form-group">
+                    <label class="col-form-label">Email Address</label>
+                    <input class="form-control" type="email" name="email" :value="old('email')" required autofocus>
+                    <x-input-error :messages="$errors->get('email')" class="mt-2 text-danger" />
+                  </div>
+                  <div class="form-group mb-0 checkbox-checked">
+                    <div class="text-end mt-3">
+                      <button class="btn btn-primary btn-block w-100" type="submit">Reset Password</button>
+                    </div>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- jquery-->
+      <script src="{{ asset('assets/js/vendors/jquery/jquery.min.js') }}"></script>
+      <!-- bootstrap js-->
+      <script src="{{ asset('assets/js/vendors/bootstrap/dist/js/bootstrap.bundle.min.js') }}" defer=""></script>
+      <script src="{{ asset('assets/js/vendors/bootstrap/dist/js/popper.min.js') }}" defer=""></script>
+      <!--fontawesome-->
+      <script src="{{ asset('assets/js/vendors/font-awesome/fontawesome-min.js') }}"></script>
+      <!-- password_show-->
+      <script src="{{ asset('assets/js/password.js') }}"></script>
+      <!-- custom script -->
+      <script src="{{ asset('assets/js/script.js') }}"></script>
     </div>
-
-    <!-- Session Status -->
-    <x-auth-session-status class="mb-4" :status="session('status')" />
-
-    <form method="POST" action="{{ route('password.email') }}">
-        @csrf
-
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            <x-primary-button>
-                {{ __('Email Password Reset Link') }}
-            </x-primary-button>
-        </div>
-    </form>
-</x-guest-layout>
+  </body>
+</html>
--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -1,30 +1,60 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
-
-        <title>{{ config('app.name', 'Laravel') }}</title>
-
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-    </head>
-    <body class="font-sans text-gray-900 antialiased">
-        <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 dark:bg-gray-900">
+<html lang="en">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CRM Login</title>
+    <!-- Favicon icon-->
+    <link rel="icon" href="{{ asset('assets/images/favicon.png ') }}" type="image/x-icon">
+    <link rel="shortcut icon" href="{{ asset('assets/images/favicon.png') }}" type="image/x-icon">
+    <!-- Google font-->
+    <link rel="preconnect" href="https://fonts.googleapis.com/">
+    <link rel="preconnect" href="https://fonts.gstatic.com/" crossorigin="">
+    <link href="https://fonts.googleapis.com/css2?family=Nunito+Sans:opsz,wght@6..12,200;6..12,300;6..12,400;6..12,500;6..12,600;6..12,700;6..12,800;6..12,900;6..12,1000&amp;display=swap" rel="stylesheet">
+    <!-- Flag icon css -->
+    <link rel="stylesheet" href="{{ asset('assets/css/vendors/flag-icon.css') }}">
+    <!-- iconly-icon-->
+    <link rel="stylesheet" href="{{ asset('assets/css/iconly-icon.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/bulk-style.css') }}">
+    <!-- iconly-icon-->
+    <link rel="stylesheet" href="{{ asset('assets/css/themify.css') }}">
+    <!--fontawesome-->
+    <link rel="stylesheet" href="{{ asset('assets/css/fontawesome-min.css') }}">
+    <!-- Whether Icon css-->
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/vendors/weather-icons/weather-icons.min.css') }}">
+    <!-- App css -->
+    <link id="color" rel="stylesheet" href="{{ asset('assets/css/color-1.css') }}" media="screen">
+    <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
+  </head>
+  <body>
+    <!-- tap on top starts-->
+    <div class="tap-top"><i class="iconly-Arrow-Up icli"></i></div>
+    <!-- tap on tap ends-->
+    <!-- login page start-->
+    <div class="container-fluid p-0">
+      <div class="row m-0">
+        <div class="col-12 p-0">    
+          <div class="login-card login-dark">
             <div>
-                <a href="/">
-                    <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-                </a>
-            </div>
-
-            <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg">
+              <div class="login-main">
                 {{ $slot }}
             </div>
+            </div>
+          </div>
         </div>
-    </body>
+      </div>
+      <!-- jquery-->
+      <script src="{{ asset('assets/js/vendors/jquery/jquery.min.js') }}"></script>
+      <!-- bootstrap js-->
+      <script src="{{ asset('assets/js/vendors/bootstrap/dist/js/bootstrap.bundle.min.js') }}" defer=""></script>
+      <script src="{{ asset('assets/js/vendors/bootstrap/dist/js/popper.min.js') }}" defer=""></script>
+      <!--fontawesome-->
+      <script src="{{ asset('assets/js/vendors/font-awesome/fontawesome-min.js') }}"></script>
+      <!-- password_show-->
+      <script src="{{ asset('assets/js/password.js') }}"></script>
+      <!-- custom script -->
+      <script src="{{ asset('assets/js/script.js') }}"></script>
+    </div>
+  </body>
 </html>
